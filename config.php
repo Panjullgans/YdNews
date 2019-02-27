9b41430ef8078ec8886c92997286765218cad5e6
Panjull Gans <48045740+Panjullgans@users.noreply.github.com>
2019-02-27 21:14:50 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+$config = [
+"imei" => "865646039377381",
+"sign" => "c9807b398e05cf73f80a6c5993150f9e17b7c06e",
+"token" => "ed47edcc7b1ed6ffc28cac905a4f3dbf",
+"uuid" => "7513e93c2c9cf6c2"
+];
+
